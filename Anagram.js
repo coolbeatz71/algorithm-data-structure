12864bf88b3fe47652bcdd3f484b57482d5397b2
coolbeatz71 <sigmacool@gmail.com>
2024-03-24 19:47:41 +0200

    Anagram: add improved solution

--- Anagram.js
+++ Anagram.js
@@ -2,6 +2,14 @@
  * Write a function that accepts two strings,
  * and return true if the strings are anagrams of each other.
  *
+ * * My Steps
+ * 1. If the two strings have different length then return 0.
+ * 2. Loop through the 1st string and return the frequency counter
+ * 3. Loop through the 2nd string and return the frequency counter
+ * 4. Loop through the frequencyCounter1 and check:
+ *    - if all the key from frequencyCounter1 exist in frequencyCounter2
+ *    - if all the 2 frequency counters have the same keys/values pair
+ *
  * @param {String} str1
  * @param {String} str2
  * @return {Boolean}
@@ -32,6 +40,33 @@ function myAttempt(str1, str2) {
 
     if (frequencyCounter2[key] !== frequencyCounter1[key]) {
       return false;
+    }
+  }
+
+  return true;
+}
+
+function Anagram(str1, str2) {
+  const lenString1 = str1.length;
+  const lenString2 = str2.length;
+
+  if (lenString1 !== lenString2) {
+    return false;
+  }
+
+  let frequencyCounter = {};
+
+  for (let char of str1) {
+    frequencyCounter[char]
+      ? (frequencyCounter[char] += 1)
+      : (frequencyCounter[char] = 1);
+  }
+
+  for (let char of str2) {
+    if (!frequencyCounter[char]) {
+      return false;
+    } else {
+      frequencyCounter[char] -= 1;
     }
   }
 
