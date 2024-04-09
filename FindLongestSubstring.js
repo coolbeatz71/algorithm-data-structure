bffe4413f798d3762a8f99f99c43225675080568
coolbeatz71 <sigmacool@gmail.com>
2024-04-09 15:37:04 +0200

    Add algorithm steps

--- FindLongestSubstring.js
+++ FindLongestSubstring.js
@@ -1,3 +1,26 @@
+/**
+ * Write a function called findLongestSubstring, which accepts a string
+ * and returns the length of the longest substring with all distinct characters
+ *
+ * * STEP
+ * This uses sling window techniques and set
+ * 1. initialize the start and end to 0;
+ * 2. Create a set, and initialize longest variable to 0;
+ * 3. add an edge if the main string is empty to return 0;
+ * 4. Create a loop while end edge is within the string lebgth;
+ * 5. if the set doesn't contain the character at end edge, add it to the set,
+ *    increment the end edge, update longest by the maximum
+ *    between the set size and the current longest;
+ * 6. if the set contains the character at end edge, delete the character at start edge
+ *    and increment the start edge;
+ *
+ * * NOTE:
+ *   Incrementing the end edge and deleting the start edge from the cumulative set
+ *   is the key to the sliding window here;
+ *
+ * @param {string} str
+ * @returns {number}
+ */
 function findLongestSubstring(str) {
   let start = 0;
   let end = 0;
--- MinSubArrayLen.js
+++ MinSubArrayLen.js
@@ -6,25 +6,39 @@
  * of which the sum is greater than or equal to the integer passed to the function.
  * If there isn't one, return 0 instead.
  *
+ * * STEP
+ * 1. add a edge case for the empty array;
+ * 2. initialize the left and right edge to 0;
+ * 3. initialize the sum to the first element of the num array
+ * 4. initialize the minimum length to the last index of the array
+ * 5. Create a loop while the while is within the array length
+ * 6. If the sum is less than the count, increment the right edge and add it to the cumulative sum
+ * 7. If the sum is greater or equal, update the min by the minimum between the current minimum and
+ *    the next (right+1) right edge and the previous left edge (-left),
+ *    remove the left edge from the cumulative sum and increment the left edge;
+ * 8. At the end, if the minimum didn't change (means It's still equal to the its initialized value)
+ *    return 0, if not return the new minimum
+ *
  * @param {number[]} numArray
  * @param {number} count
  * @return {boolean}
  */
 function minSubArrayLen(numArray, count) {
   if (numArray.length === 0) return 0;
+
   let left = 0;
   let right = 0;
   let sum = numArray[0];
   let min = numArray.length + 1;
 
   while (right < numArray.length) {
-    if (sum >= count) {
-      min = Math.min(min, right - left + 1);
-      sum -= numArray[left];
-      left++;
-    } else {
+    if (sum < count) {
       right++;
       sum += numArray[right];
+    } else {
+      min = Math.min(min, right + 1 - left);
+      sum -= numArray[left];
+      left++;
     }
   }
 
